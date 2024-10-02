76783998e2c3aeb0bfe8b980af4dd958e2429581
Jose Javier Gálvez Gómez <jpromocion@gmail.com>
2024-10-02 17:31:36 +0200

    Mostrado en about de version tambien de la api rest

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { version } from '../../../package.json';
+import { DemoService } from '../demo.service';
+
 @Component({
   selector: 'app-about',
   standalone: true,
@@ -8,8 +10,27 @@ import { version } from '../../../package.json';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
-  version = version;
+  versionApp = version;
+
+  versionApiRest = '';
+
+  //inyeccion de dependencia para utilizar el servicio de generacion de nifs
+  private demoService: DemoService = inject(DemoService);
+
+  ngOnInit(): void {
+    this.fijarVersionAPI();
+
+  }
+
+
+  fijarVersionAPI(): void {
+    this.versionApiRest = '';
+    this.demoService.getVersion()
+    .subscribe(versionReturn => {
+      this.versionApiRest = versionReturn;
+    });
+  }
 
 }
---
+++ src/app/demo.service.ts
@@ -0,0 +1,101 @@
+/**
+ * La clase que actuara coimo servicio para acceder a nuestra itnerfaz
+ * API rest "demo/"
+ */
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of, delay } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+import { MessageService } from './message.service';
+import { DatosConexionService } from './datos-conexion.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DemoService {
+
+  //La URL de la API rest
+  private urlJsonServer = environment.apiUrl;
+  private interfaz = '/demo';
+
+  baseHeaders = new HttpHeaders().set('X-API-KEY', '');
+
+  //inyectamos el servicio HTTP
+  private http: HttpClient = inject(HttpClient);
+  //inyeccion de dependencias para que a su vez pueda hacer uso del servicio de mensajes
+  messageService: MessageService = inject(MessageService);
+
+  //inyectamos el servicio de datos conexion, para obtener la api-key que fija el componente padre
+  //de todo app
+  private datosConexionService: DatosConexionService = inject(DatosConexionService);
+
+  constructor () {
+  }
+
+  /**
+   * Fijar la apyKey
+   * @param apiKeyIn
+   */
+  private setApiKey(apiKeyIn: string) {
+    this.baseHeaders = new HttpHeaders().set('X-API-KEY', apiKeyIn);
+  }
+
+  /**
+   * Fijar la api-key del servicio de datos conexion
+   */
+  private fijarApiKeyServicio() {
+    this.setApiKey(this.datosConexionService.getApiKey());
+  }
+
+  /** Log a HeroService message with the MessageService */
+  /**
+   * Loguear un mensaje en el servicio de mensajes
+   * @param message
+   */
+  private log(message: string, error: boolean = false) {
+    console.info(`DemoService: ${message}`);
+    if (error) {
+      this.messageService.addError(`DemoService: ${message}`);
+    } else {
+      this.messageService.add(`DemoService: ${message}`);
+    }
+  }
+
+  /**
+  * Manjear fallo en operación Http
+  * Mantiene la app en funcionamiento.
+  *
+  * @param operation - nombre de la operación fallada
+  * @param result - valor opcional a retornar como resultado observable
+  */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); // log to console instead
+
+      this.log(`${operation} fallo: ${error.message} - Error adicional: ${error.error}`, true);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /**
+   * Interfaz de invocación del servicio rest para obtener la version
+   * Interfaz: GET /demo/version
+   * @returns Lista de NIFS generados aleatoriamente
+   */
+  getVersion(): Observable<string>  {
+    //fijamos la api-key del servicio de datos conexion
+    this.fijarApiKeyServicio();
+    return this.http.get(this.urlJsonServer + this.interfaz + '/version', {
+      headers: this.baseHeaders, responseType: 'text'
+    })
+      .pipe(
+        //tap(_ => this.log('Nifs recuperados')),
+        catchError(this.handleError<string>('getVersion', ''))
+      );
+  }
+
+
+}
