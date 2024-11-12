161bc8a784f56bc019580423997a927cd42753e2
Jose Javier Gálvez Gómez <jpromocion@gmail.com>
2024-11-12 09:58:23 +0100

    about: Menu social compartir en forma de glow

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -19,8 +19,36 @@ export class AboutComponent implements OnInit {
   //inyeccion de dependencia para utilizar el servicio de generacion de nifs
   private demoService: DemoService = inject(DemoService);
 
+  /**
+   * INicializar los elementos del card glowes social
+   */
+  private inicializarCardSocial(): void {
+    const shareBtn = document.querySelector(".card-social-share-btn");
+    const card1 = document.querySelector(".card-social-card1");
+    const card2 = document.querySelector(".card-social-card2");
+    const card3 = document.querySelector(".card-social-card3");
+    const card4 = document.querySelector(".card-social-card4");
+
+
+    shareBtn?.addEventListener("click", () => {
+        if (card1?.getAttribute("data-toggle1") === "true") {
+            card1.setAttribute("data-toggle1", 'false');
+            card2?.setAttribute("data-toggle2", 'false');
+            card3?.setAttribute("data-toggle3", 'false');
+            card4?.setAttribute("data-toggle4", 'false');
+        } else if (card1?.getAttribute("data-toggle1") === "false") {
+            card1.setAttribute("data-toggle1", 'true');
+            card2?.setAttribute("data-toggle2", 'true');
+            card3?.setAttribute("data-toggle3", 'true');
+            card4?.setAttribute("data-toggle4", 'true');
+        }
+    });
+  }
+
   ngOnInit(): void {
     this.fijarVersionAPI();
+
+    this.inicializarCardSocial();
 
   }
 
