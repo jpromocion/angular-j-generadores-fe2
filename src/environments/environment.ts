52ad718d73a94ddea501a896a5485639cf8f53ed
Jose Javier Gálvez Gómez <jpromocion@gmail.com>
2024-11-18 09:06:14 +0100

    prueba: api rest url por variable entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rest-j-generadores-production.up.railway.app/generadores'
+  //apiUrl: 'https://rest-j-generadores.onrender.com/generadores'
+  //apiUrl: 'https://rest-j-generadores-production.up.railway.app/generadores'
+  apiUrl: process.env["API_REST_PROD"]
 };
