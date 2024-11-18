55adf304420b1bf649de23776ce3137a5688a8cb
Jose Javier Gálvez Gómez <jpromocion@gmail.com>
2024-11-18 09:36:12 +0100

    restablecer apir rest

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://rest-j-generadores.onrender.com/generadores'
-  //apiUrl: 'https://rest-j-generadores-production.up.railway.app/generadores'
-  apiUrl: process.env["API_REST_PROD"]
+  apiUrl: 'https://rest-j-generadores-production.up.railway.app/generadores'
 };
